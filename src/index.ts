4e65e3d7e6cafed244d3bb02465c0ac7eb440f44
jjrv <juha@busfaster.com>
2017-08-02 20:53:45 +0300

    Export some useful types.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
 // This file is part of geotree, copyright (c) 2017 BusFaster Ltd.
 // Released under the MIT license, see LICENSE.
 
-export { QuadTree } from './QuadTree';
+export { QuadTree, QuadTile, QuadPos, QuadType } from './QuadTree';
